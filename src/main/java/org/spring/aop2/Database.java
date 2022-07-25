0716c944367bf359c4970d0a9a04f3f358036a27
Ahmed  Elsheikh <ahmedelsheikh680@gmail.com>
2022-07-25 06:48:48 -0700

    pointcut #6

--- src/main/java/org/spring/aop2/Database.java
+++ src/main/java/org/spring/aop2/Database.java
@@ -2,14 +2,25 @@ package org.spring.aop2;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class Database {
 
-    @Before("execution( * org.spring.dao.*.*(..)))")
+    @Pointcut("execution( * org.spring.dao.*.*(..))")
+    public void myPointCut(){
+
+    }
+    @Before("myPointCut())")
     public void connectToDB() {
         System.out.println("Connected!!!!!!!");
+    }
+
+//    @Before("execution(* org.spring.service.*.*(..))")
+    @Before("myPointCut()")
+    public void login() {
+        System.out.println("Login...................True");
     }
 }
